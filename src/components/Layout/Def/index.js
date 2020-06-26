6bf698307e84ae1f9e825660377da20a7937dce0
Hansanghyeon <dandan9509@gmail.com>
2020-06-27 00:39:04 +0900

    chore: 시나리오 레이아웃 임시

--- src/components/Layout/Def/index.js
+++ src/components/Layout/Def/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 // Components
 import { Container, Row, Col } from './index.style';
+import List from '@atom/list/Def';
+import Popup from '@molecule/Popup';
+import CreatePost from '@molecule/CreatePost';
 
 const DefLayout = () => {
   return (
     <Container.Board>
-      <Row.Def>
-        <Col.Def>test</Col.Def>
-      </Row.Def>
+      <List number="번호" title="제목" author="작성자" date={null} />
+      <Popup>
+        <CreatePost title="시니어 계획 등록" />
+      </Popup>
     </Container.Board>
   );
 };
