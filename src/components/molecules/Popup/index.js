22c998882d108765b8fdee57ffd532d9da26ae82
Hansanghyeon <dandan9509@gmail.com>
2020-06-27 17:02:34 +0900

    feat: 팝업 open/close 메소드 생성

--- src/components/molecules/Popup/index.js
+++ src/components/molecules/Popup/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { TimelineLite } from 'gsap';
 // components
 import { PupupWrap, Container } from './index.style';
+import useOutsideAlerter from '@utile/useOutsideAlerter';
 
 const Popup = ({ children }) => {
+  const MainContainer = useRef();
+  const Popup = useRef();
+  const closedPopup = () => {
+    const tl = new TimelineLite();
+    tl.to(Popup.current, 0, {
+      display: 'none',
+    });
+  };
+  const OpenPopup = () => {
+    const tl = new TimelineLite();
+    tl.to(Popup.current, 0, {
+      display: null,
+    });
+  };
+  useOutsideAlerter({ ref: MainContainer, inFunction: closedPopup });
   return (
-    <PupupWrap>
-      <Container.Def>{children}</Container.Def>
+    <PupupWrap ref={Popup}>
+      <Container.Def ref={MainContainer}>{children}</Container.Def>
     </PupupWrap>
   );
 };
