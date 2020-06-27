c7eaac96077c4f3a090e2d5c25047435594f9293
Hansanghyeon <dandan9509@gmail.com>
2020-06-27 15:29:57 +0900

    feat: list 헤더레이아웃 맞춤 자동

--- src/components/atoms/list/Def/index.js
+++ src/components/atoms/list/Def/index.js
@@ -6,10 +6,10 @@ import { Row, Col } from './index.style';
 const List = ({ number, title, author, date }) => {
   return (
     <Row.Def>
-      <Col.Def col={1}>{number}</Col.Def>
+      <Col.Def auto>{number}</Col.Def>
       <Col.Def col>{title}</Col.Def>
-      <Col.Def col={2}>{author}</Col.Def>
-      <Col.Def col={2}>
+      <Col.Def auto>{author}</Col.Def>
+      <Col.Def auto>
         {date ? moment(date).format('YYYY.MM.DD') : '작성일'}
       </Col.Def>
     </Row.Def>
